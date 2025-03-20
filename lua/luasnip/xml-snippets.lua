cb6a726295c235da48868a05888fd5d013e97afb
dbhatfield <danielbrucehatfield@gmail.com>
2025-03-19 19:04:23 -0700

    Snippet Changes

--- lua/luasnip/xml-snippets.lua
+++ lua/luasnip/xml-snippets.lua
@@ -5,13 +5,14 @@ local s = ls.snippet
 local i = ls.insert_node
 local rep = extras.rep
 local fmt = require('luasnip.extras.fmt').fmt
+local c = ls.choice_node
 
 ls.add_snippets('xml', {
 
   -- General purpose definitions
 
   s(
-    '<block',
+    'block',
     fmt(
       [[
       <{}>
@@ -28,45 +29,117 @@ ls.add_snippets('xml', {
 
   -- TOT snippet definitions
 
-  s(';newline', t '&#xA;'),
+  s('newline', t '&#xD;&#xA;'),
 
   s(
-    '<attr',
+    'attr',
     fmt(
       [[
-      <Attribute {}="{}" {}/>
+      <Attribute ID="{}" Value1="{}" />
       ]],
       {
         i(1),
         i(2),
-        i(3),
       }
     )
   ),
 
   s(
-    '<desc',
+    'desc',
     fmt(
       [[
-      <Object ID="{}" DisplayName="{}" Type="{}">
-        {}
+      <Object ID="{}" DefaultValue="" DisplayValue="{}" Type="{}" Value="">
+        <Attribute ID="Required" Value1="{}" />
+        <Attribute ID="Visible" Value1="{}" />
+        <Attribute ID="GUIForceUppercaseTranslation" Value1="True" />
+        <Attribute ID="GroupString" Value1="{}" />
+        <Attribute ID="ObjectOrder" Value1="{}" />
       </Object>
       ]],
       {
         i(1),
         i(2),
-        i(3),
-        i(4),
+        c(3, {
+          t '1',
+          t '2',
+        }),
+        c(4, {
+          t 'False',
+          t 'True',
+        }),
+        c(5, {
+          t 'False',
+          t 'True',
+        }),
+        i(6),
+        i(7),
       }
     )
   ),
 
   s(
-    '<tl-obj',
+    'desc-date',
     fmt(
       [[
-    <Object ID="{}" Value1="{}">
-      <AddDescriptorObject ID="{}" />
+      <Object ID="{}" DefaultValue="" DisplayValue="{}" Type="{}" Value="">
+        <Attribute ID="DateFormat" Value1="{}" />
+        <Attribute ID="Required" Value1="{}" />
+        <Attribute ID="Visible" Value1="{}" />
+        <Attribute ID="GUIForceUppercaseTranslation" Value1="True" />
+        <Attribute ID="GroupString" Value1="{}" />
+        <Attribute ID="ObjectOrder" Value1="{}" />
+        <Attribute ID="RegularExpressionConstraint" Value1="[0-9]*" />
+        <RuleSet ID="12" Type="0" />
+        <RuleSet ID="14" Type="0" />
+      </Object>
+      ]],
+      {
+        i(1),
+        i(2),
+        c(3, {
+          t '1',
+          t '2',
+        }),
+        c(4, {
+          t 'DDMMCCYY',
+          t 'MMYYMMDD',
+        }),
+        c(5, {
+          t 'False',
+          t 'True',
+        }),
+        c(6, {
+          t 'False',
+          t 'True',
+        }),
+        i(7),
+        i(8),
+      }
+    )
+  ),
+
+  s(
+    'workflow-obj',
+    fmt(
+      [[
+    <Object ID="{}" Type="{}" />
+      ]],
+      {
+        i(1),
+        c(2, {
+          t '1',
+          t '2048',
+        }),
+      }
+    )
+  ),
+
+  s(
+    'nist',
+    fmt(
+      [[
+    <Object ID="{}" BoundIDArray0="{}">
+      <AddDescriptorObjectValue Value="{}" ForceUppercase="True" />
     </Object>
     ]],
       {
@@ -78,17 +151,16 @@ ls.add_snippets('xml', {
   ),
 
   s(
-    '<tl-obj',
+    'nist-delegate',
     fmt(
       [[
-    <Object ID="{}" Value1="{}">
-      <AddDelegate ID="{}" />
+    <Object ID="{}">
+      <AddDelegate Value="{}" ForceUppercase="True" />
     </Object>
     ]],
       {
         i(1),
         i(2),
-        i(3),
       }
     )
   ),
